88509dd4d2340739d516e96b15b0fdb30040481a
eka-ant16 <muktieka44@gmail.com>
2020-10-15 17:45:38 +0700

    first commit

---
+++ app/src/main/java/com/eka/mytestingapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.eka.mytestingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnSetValue: Button
+    private lateinit var tvText: TextView
+    private lateinit var imgPreview: ImageView
+
+    private var names = ArrayList<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        imgPreview = findViewById(R.id.img_preview)
+        imgPreview.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.fronalpstock_big))
+        Glide.with(this).load(R.drawable.fronalpstock_big).into(imgPreview)
+
+        btnSetValue = findViewById(R.id.btn_set_value)
+        tvText = findViewById(R.id.tv_text)
+
+        btnSetValue.setOnClickListener(this)
+
+        names.add("Narenda Wicaksono")
+        names.add("Kevin")
+        names.add("Yoza")
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_set_value) {
+            Log.d("MainActivity", names.toString())
+            val name = StringBuilder()
+            for (i in 0..2) {
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
+        }
+    }
+}
